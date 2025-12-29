079e91922448c0479271179d25b0e6a653a02f17
avinashkrdss3-source <avinashkrdss3@gmail.com>
2025-12-29 08:23:22 +0530

    Create chainparams.cpp

---
+++ src/chainparams.cpp
@@ -0,0 +1,5 @@
+strNetworkID = "main";
+consensus.nSubsidyHalvingInterval = 210000; // Har 210,000 blocks par reward half hoga
+consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+consensus.nPowTargetTimespan = 3.5 * 24 * 60 * 60; // 3.5 days
+consensus.nPowTargetSpacing = 2.5 * 60; // Har 2.5 minute mein 1 block
